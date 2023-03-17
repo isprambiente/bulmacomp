e34e715b0f45235ce5d9cf5893da5f92710186a9
MDreW <andrea.ranaldi@gmail.com>
2023-03-17 16:12:18 +0100

    commenti panel_component

--- app/components/bulmacomp/panel_component.rb
+++ app/components/bulmacomp/panel_component.rb
@@ -1,18 +1,45 @@
 # frozen_string_literal: true
 
 module Bulmacomp
+  # Make an html structure for a bulma breadcrumb
+  #
+  # @example Empty panel:
+  #   render Bulmacomp::PanelComponent.new()
+  #
+  #   <nav class="panel">
+  #   </nav>
+  #
+  # @example panel with option:
+  #   = render Bulmacomp::PanelComponent.new(title: 'test title') do
+  #     <a class="panel-block is-active">
+  #       <span class="panel-icon"><i class="fas fa-book" aria-hidden="true"></i></span>
+  #       bulma
+  #     </a>
+  #
+  #   <nav class="panel">
+  #     <p class="panel-heading">test title</p>
+  #     <a class="panel-block is-active">
+  #       <span class="panel-icon"><i class="fas fa-book" aria-hidden="true"></i></span>
+  #       bulma
+  #     </a>
+  #   </nav>
   class PanelComponent < ViewComponent::Base
+    # @param [Hash] opts options to generate content
+    # @option opts [String] :title panel title
+    # @option opts [String] :* each other key going as tag option, default is class: 'breadcrumb', aria_label: 'breadcrumbs'
+    # @yield [optional] panel content
     def initialize(title: nil, **opts)
       @title = title
       @opts = opts
       @opts[:class] = 'panel' unless @opts[:class]
     end
 
-    # return generated bulma nav object
+    # return [String] html_safe generated bulma panel
     def call
       content_tag :nav, safe_join([title, content]), **@opts
     end
 
+    # return [String] html_safe generated panel title
     def title
       tag.p @title, class: 'panel-heading' if @title
     end
